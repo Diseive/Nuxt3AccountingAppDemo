716a4be88d979fd6712627133d64d2a609bec25d
Diseive <104061331+Diseive@users.noreply.github.com>
2023-10-23 05:57:25 +0000

    complex

---
+++ pages/demo/complex-journal.vue
@@ -0,0 +1,32 @@
+<template>
+    <p>複合仕訳入力</p>
+<div class="container">
+    <h1>仕訳帳</h1>
+  <table class="table">
+    <thead>
+      <tr>
+        <th>年度</th>
+        <th>区分</th>
+        <th>会計単位</th>
+        <th>消費税</th>
+        <th>対象期間</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td></td>
+        <td></td>
+      </tr>
+      <tr>
+        <td></td>
+        <td></td>
+      </tr>
+      <tr>
+        <td></td>
+        <td></td>
+      </tr>
+    </tbody>
+  </table>
+</div>
+
+</template>
